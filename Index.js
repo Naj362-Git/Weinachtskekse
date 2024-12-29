0e452b90e87b5c936850eb08c13ca77b45dfa30c
Naj362-Git <Jan.Sonnenschein@outlook.com>
2024-12-29 12:55:29 +0100

    JavaScript verändert, Css updated, Html angepasst
    JavaScript 90% der Mathe ist da
    Kleine Css Änderungen
    Text Feld für den Rabbat-code in Html

--- Index.js
+++ Index.js
@@ -1,15 +1,22 @@
 let base_price = 0;
+let total_price = 0;
 let shipping = 0;
 let total = 0;
-let Vat = 0;
 
-let boxgröße_P = 2;
-let keksform_P = 4;
-let K_topping_P = 5;
-let E_topping_P = 6;
-let verpakung_P = 3;
-let optional_P = 2;
+let MwSt = 0;
+const MwSt_Satz = 0.07;
 
+const r_code = XMAS2024;
+const r_Rabatt = 0.1;
+
+let boxgröße_P = 0;
+let keksform_P = 0;
+let K_topping_P = 20;
+let E_topping_P = 0;
+let verpakung_P = 0;
+let optional_P = 0;
+
+//Basispreisberechnung
 function base_price_calc() {
   base_price =
     boxgröße_P +
@@ -21,5 +28,41 @@ function base_price_calc() {
 }
 base_price_calc();
 
-base_price = console.log(base_price);
-console.log(typeof base_price);
+// Versandkostenberechnung
+function shipping_calc() {
+  if (base_price > 0 && base_price < 20) {
+    shipping = 3.99;
+  } else if (base_price >= 20 && base_price < 50) {
+    shipping = 1.8;
+  } else shipping = 0;
+}
+shipping_calc();
+
+// Gesamtpreisberechnung
+function total_price_calc() {
+  total_price = base_price + shipping;
+}
+total_price_calc();
+
+console.log(total_price);
+
+//Mehrwertsteuerberechnung
+function MwSt_calc() {
+  MwSt = total_price * MwSt_Satz;
+
+  MwSt = Math.round(MwSt * 100) / 100;
+}
+MwSt_calc();
+
+//Umformatierung
+function format_number(num) {
+  return new Intl.NumberFormat("de-DE", {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  }).format(num);
+}
+
+console.log("Basispreis:", format_number(base_price)); // Base price
+console.log("Versandkosten:", format_number(shipping)); // Shipping cost
+console.log("Gesamtpreis:", format_number(total_price)); // Total price
+console.log("MwSt:", format_number(MwSt)); // VAT
