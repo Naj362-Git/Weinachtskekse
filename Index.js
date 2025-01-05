11008e105868d0f9af3f225f1d308817bb5e501a
Naj362-Git <Jan.Sonnenschein@outlook.com>
2025-01-05 12:54:54 +0100

    JS Und HTML Fast ganz verknüpft

--- Index.js
+++ Index.js
@@ -10,19 +10,106 @@ const r_code = "XMAS2024";
 const r_Rabatt = 0.1;
 
 let boxgröße_P = 0;
+let boxgröße = 0;
 let keksform_P = 0;
-let K_topping_P = 20;
+let teigart_P = 0;
+let K_topping_P = 0;
 let E_topping_P = 0;
 let verpakung_P = 0;
 let optional_P = 0;
 
-//Überprüfen welche box ausgewählt wurde (boxgröße_P)
+//Überprüfen welche Option ausgewählt wurde (boxgröße_P)
 function boxsize() {
-  const boxradios = document.getElementsByName("boxSize");
+  const Box_radios = document.getElementsByName("boxSize");
 
-  for (const radio of boxradios) {
+  for (const radio of Box_radios) {
     if (radio.checked) {
       boxgröße_P = parseFloat(radio.value);
+      break;
+    }
+  }
+  if (boxgröße_P == 5) {
+    boxgröße = 15;
+  } else if (boxgröße_P == 7) {
+    boxgröße = 21;
+  } else {
+    boxgröße = 27;
+  }
+  price_calc();
+}
+// Überprüfen welche Option ausgewählt wurde (teigart_P)
+function Teigart() {
+  const teigartRadios = document.getElementsByName("Teig");
+
+  for (const radio of teigartRadios) {
+    if (radio.checked) {
+      teigart_P = parseFloat(radio.value);
+      break;
+    }
+  }
+  price_calc();
+}
+
+//Überprüfen welche Option ausgewählt wurde (keksform_P)
+function Keksform() {
+  const Keksform_radios = document.getElementsByName("shape");
+
+  for (const radio of Keksform_radios) {
+    if (radio.checked) {
+      keksform_P = parseFloat(radio.value);
+      break;
+    }
+  }
+  keksform_P = keksform_P * boxgröße;
+  price_calc();
+}
+
+// Überprüfen welche Option ausgewählt wurde (K_topping_P)
+function kTopping() {
+  const kToppingRadios = document.getElementsByName("kTopping");
+
+  for (const radio of kToppingRadios) {
+    if (radio.checked) {
+      K_topping_P = parseFloat(radio.value);
+      break;
+    }
+  }
+  price_calc();
+}
+
+// Überprüfen welche Option ausgewählt wurde (E_topping_P)
+function eTopping() {
+  const eTopping_Checkboxes = document.getElementsByName("extraTopping");
+
+  for (const checkbox of eTopping_Checkboxes) {
+    if (checkbox.checked) {
+      E_topping_P += parseFloat(checkbox.value);
+      break;
+    }
+  }
+  price_calc();
+}
+
+// Überprüfen welche Option ausgewählt wurde (verpakung_P)
+function verpakung() {
+  const verpakungRadios = document.getElementsByName("verpackung");
+
+  for (const radio of verpakungRadios) {
+    if (radio.checked) {
+      verpakung_P = parseFloat(radio.value);
+      break;
+    }
+  }
+  price_calc();
+}
+
+// Überprüfen welche Option ausgewählt wurde (optional_P)
+function optional() {
+  const optional_Checkboxes = document.getElementsByName("Optional");
+
+  for (const checkbox of optional_Checkboxes) {
+    if (checkbox.checked) {
+      optional_P += parseFloat(checkbox.value);
       break;
     }
   }
